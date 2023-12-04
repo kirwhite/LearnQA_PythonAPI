d7202f661463b3c46774f9211ffa7346773e34a3
kirwhite <146930891+kirwhite@users.noreply.github.com>
2023-12-04 17:15:19 +0300

    add library

--- lib/assertions.py
+++ lib/assertions.py
@@ -44,3 +44,4 @@ class Assertions:
     def assert_code_status(response: Response, expected_status_code):
         assert  response.status_code == expected_status_code, \
             f"Unexpected status code! Expected: {expected_status_code}. Actual: {response.status_code}"
+        
